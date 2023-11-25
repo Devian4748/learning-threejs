4419e2de4adfb88f57ce91ef8f0c7f5b31abe182
Devian <devian4748@gmail.com>
2023-11-26 06:02:02 +0900

    bugfix: fix vite config - include firstProject html

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
 export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
-    {
-        host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+  root: "src/",
+  publicDir: "../static/",
+  base: "./",
+  server: {
+    host: true, // Open to local network and display URL
+    open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env), // Open if it's not a CodeSandbox
+  },
+  build: {
+    outDir: "../dist", // Output in the dist/ folder
+    emptyOutDir: true, // Empty the folder first
+    sourcemap: true, // Add sourcemap
+    rollupOptions: {
+      input: {
+        main: "src/index.html",
+        firstProject: "src/pages/first-project/index.html",
+        // Add other entry points here if needed
+      },
     },
-    build:
-    {
-        outDir: '../dist', // Output in the dist/ folder
-        emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
-    },
-}
+  },
+};
